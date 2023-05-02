4200add5fe23316f6c7fe61a6ecbb24f03ac1455
Shubham <shubhamvscode@gmail.com>
2023-05-02 20:02:43 +0530

    input border changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -106,7 +106,7 @@ export default function LoginPage() {
                 type="email"
                 id="email"
                 {...register("email")}
-                className={`border px-3 py-2 rounded focus:outline-2 outline-primaryBlue ${
+                className={`border border-black/30 px-3 py-2 rounded focus:outline-2 outline-primaryBlue ${
                   errors.email?.message &&
                   "border-2 border-red-400 outline-red-400"
                 }`}
@@ -122,7 +122,7 @@ export default function LoginPage() {
                 type="password"
                 id="password"
                 {...register("password")}
-                className={`border px-3 py-2 rounded focus:outline-2 outline-primaryBlue ${
+                className={`border border-black/30 px-3 py-2 rounded focus:outline-2 outline-primaryBlue ${
                   errors.password?.message &&
                   "border-2 border-red-400 outline-red-400"
                 }`}
